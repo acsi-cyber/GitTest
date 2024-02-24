dbcfeaa9aa8abdb017f7f699fb2f5ce092b90ed4
acsi-cyber <119968594+acsi-cyber@users.noreply.github.com>
2024-02-24 19:57:01 +0500

    v_0.1
    Load project

---
+++ GitTest2/Form1.cs
@@ -0,0 +1,40 @@
+﻿using form3;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GitTest2
+{
+    public partial class Form1 : Form
+    {
+        public static string text1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 frm2 = new Form2();
+            frm2.label1.Text = this.textBox1.Text;
+            frm2.Show();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
